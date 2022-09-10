f589ae0b4418372560a96d02b8579c080868ef69
Alex Wibowo <alexwibowo@gmail.com>
2022-09-10 21:59:23 +1000

    Adding fetching remote data

--- Home.js
+++ Home.js
@@ -1,20 +1,89 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  FlatList,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
 
-export default function Homepage() {
+// since homepage is added to navigation, it has access to navigatin prop
+export default function Homepage({ navigation }) {
+  const [dataLoading, finishLoading] = useState(true);
+  const [newsData, setData] = useState([]);
+
+  // combination of componentDidMount, componentDidUpdate, componentWillUnmount lifecycles
+  // empty array to indicate that it should just run once, and will never update after we get them
+  useEffect(() => {
+    fetch(
+      "https://newsapi.org/v2/everything?q=tech&apiKey=ca6f72a0379e45938a5fe4bb0876deba"
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json.articles))
+      .catch((error) => console.error(error))
+      .finally(() => finishLoading(false));
+  }, []);
+
+  const storyItem = ({ item }) => {
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => {
+          navigation.navigate("NewsDetail", { url: item.url });
+        }}
+      >
+        <View style={styles.listings}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Image style={styles.thumbnail} source={{ uri: item.urlToImage }} />
+          <Text style={styles.blurb}>{item.description}</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>This will be the Home Page</Text>
+      {dataLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={newsData}
+          renderItem={storyItem}
+          keyExtractor={(item) => item.url}
+        />
+      )}
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    padding: 20,
+  },
+  thumbnail: {
+    height: 100,
 
-const styles=StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+    width: "98%",
+  },
+  listings: {
+    paddingTop: 15,
+    paddingBottom: 25,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+  title: {
+    paddingBottom: 10,
+    fontFamily: "OpenSans",
+    fontWeight: "bold",
+  },
+  blurb: {
+    fontFamily: "OpenSans",
+    fontStyle: 'italic',    
+  }
+});
